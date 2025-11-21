c38c440034e4e0904c89d7f7b01fe4c8c63ec845
Marco <marcodiano72@gmail.com>
2025-11-21 15:09:46 +0100

    First commit

---
+++ src/main/java/it/unisa/diem/gruppo23/App.java
@@ -0,0 +1,10 @@
+package it.unisa.diem.gruppo23;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
