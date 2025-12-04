d5165640173f10d1790e1c02a8ea3859c6d3c0d6
nazzato-hub <n.azzato@studenti.unisa.it>
2025-12-04 16:13:17 +0100

    Modifica classi

---
+++ src/main/java/it/unisa/diem/gruppo01/interfacce/Interfaccia_nuovoPrestitoController.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.unisa.diem.gruppo01.interfacce;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author nicolaazzato
+ */
+public class Interfaccia_nuovoPrestitoController implements Initializable {
+
+    @FXML
+    private TextField inserisciM;
+    @FXML
+    private TextField inserisciN;
+    @FXML
+    private TextField inserisciC;
+    @FXML
+    private TextField inserisciE;
+    @FXML
+    private CheckBox spuntaU;
+    @FXML
+    private TextField inserisciT;
+    @FXML
+    private TextField inserisciA;
+    @FXML
+    private CheckBox spuntaL;
+    @FXML
+    private Button salvaP;
+    @FXML
+    private TextField inserisciMa;
+    @FXML
+    private TextField inserisciISBN;
+    @FXML
+    private Button salvaRes;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void inserisciMatricola(ActionEvent event) {
+    }
+
+    @FXML
+    private void inserisciNome(ActionEvent event) {
+    }
+
+    @FXML
+    private void inserisciCognome(ActionEvent event) {
+    }
+
+    @FXML
+    private void inserisciEmail(ActionEvent event) {
+    }
+
+    @FXML
+    private void spuntaUtente(ActionEvent event) {
+    }
+
+    @FXML
+    private void inserisciTitolo(ActionEvent event) {
+    }
+
+    @FXML
+    private void inserisciAutore(ActionEvent event) {
+    }
+
+    @FXML
+    private void spuntaLibro(ActionEvent event) {
+    }
+
+    @FXML
+    private void salvaPrestito(ActionEvent event) {
+    }
+
+    @FXML
+    private void inserisceMatricola(ActionEvent event) {
+    }
+
+    @FXML
+    private void inserisceISBN(ActionEvent event) {
+    }
+
+    @FXML
+    private void salvaRestituzione(ActionEvent event) {
+    }
+    
+}
