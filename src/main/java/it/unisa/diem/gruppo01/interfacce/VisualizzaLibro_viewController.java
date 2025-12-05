d7b1750d5f6ec80619305adb151882b9086346db
macucc <macucc@localhost>
2025-12-05 14:34:23 +0100

    inserimento interfacce

---
+++ src/main/java/it/unisa/diem/gruppo01/interfacce/VisualizzaLibro_viewController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package it.unisa.diem.gruppo01.interfacce;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author macucc
+ */
+public class VisualizzaLibro_viewController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
