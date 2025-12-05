6a69115ab0aeb46c6d786a1f87d92709ba32037d
nazzato-hub <n.azzato@studenti.unisa.it>
2025-12-05 12:52:34 +0100

    Modifica classi

---
+++ src/main/java/it/unisa/diem/gruppo01/interfacce/InterfacciaMenu_viewController.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author nicolaazzato
+ */
+public class InterfacciaMenu_viewController implements Initializable {
+
+    @FXML
+    private Button gestioneLButton;
+    @FXML
+    private Button gestioneSButton;
+    @FXML
+    private Button gestionePButton;
+    @FXML
+    private Button gestioneEButton;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void openLibriInterface(ActionEvent event) {
+    }
+
+    @FXML
+    private void openStudentInterface(ActionEvent event) {
+    }
+
+    @FXML
+    private void openPrestitiInterface(ActionEvent event) {
+    }
+
+    @FXML
+    private void openExitInterface(ActionEvent event) {
+    }
+    
+}
